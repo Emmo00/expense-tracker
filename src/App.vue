42b64dbf15576e153aa7aff455d09f092d392c16
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-02-08 18:53:20 +0100

    setup and templates

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <HeaderVue />
+    <div class="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+    </div>
+</template>
+
+<script setup>
+import HeaderVue from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+</script>
